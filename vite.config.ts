2d01efb33c4d18763235b3142b7be922ab16077c
Pier-Olivier Létourneau <111366978+PIMPOON@users.noreply.github.com>
2025-10-29 22:09:19 -0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,54 +1,54 @@
-// // import { defineConfig } from 'vite'
-// // import react from '@vitejs/plugin-react-swc'
-// // import path from "path";
-// // import { Plugin } from 'vite';
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 // import path from "path";
-// import { componentTagger } from "lovable-tagger";
+// import { Plugin } from 'vite';
 
-// // https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: "/cocktails/",
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { fileURLToPath, URL } from 'node:url'
+
+// // https://vite.dev/config/
 // export default defineConfig(({ mode }) => ({
 //   base: mode === "development" ? "/" : "/cocktails/",
 //   server: {
 //     host: "::",
 //     port: 8080,
 //   },
-//   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+//   plugins: [react()],
 //   resolve: {
 //     alias: {
-//       "@": path.resolve(__dirname, "./src"),
+//       '@': fileURLToPath(new URL('./src', import.meta.url)),
 //     },
 //   },
+//   build: {
+//     outDir: 'dist', // Default output directory
+//     rollupOptions: {
+//       output: {
+//         entryFileNames: 'assets/[name].js',
+//         chunkFileNames: 'assets/[name].js',
+//         assetFileNames: 'assets/[name].[ext]'
+//       }
+//     }
+//   }
 // }));
-
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
-
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/cocktails/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  build: {
-    outDir: 'dist', // Default output directory
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  }
-}));
