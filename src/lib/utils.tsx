42779b389653cc5ced762d99baaae14453a01a0e
Pier-Olivier Létourneau <111366978+PIMPOON@users.noreply.github.com>
2025-10-29 22:11:31 -0400

    update

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -4,31 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export interface Drink {
-  name: string;
-  image: string;
-  ingredients: string[];
-  instructions?: string;
-}
-
-export interface CardData {
-  el: HTMLElement;
-  tags: string[];
-}
-
-export interface AlcoholMap {
-  [key: string]: string[];
-}
-
-export interface Cocktail {
-name: string;
-image: string;
-ingredients: string[];
-url: string;
-instructions?: string;
-}
-
-export interface CocktailWithTags extends Cocktail {
-alcohols: string[];
-}
--- vite.config.ts
+++ vite.config.ts
@@ -3,52 +3,48 @@
 // import path from "path";
 // import { Plugin } from 'vite';
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/cocktails/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import { fileURLToPath, URL } from 'node:url'
-
-// // https://vite.dev/config/
+// // https://vitejs.dev/config/
 // export default defineConfig(({ mode }) => ({
 //   base: mode === "development" ? "/" : "/cocktails/",
 //   server: {
 //     host: "::",
 //     port: 8080,
 //   },
-//   plugins: [react()],
+//   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
 //   resolve: {
 //     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url)),
+//       "@": path.resolve(__dirname, "./src"),
 //     },
 //   },
-//   build: {
-//     outDir: 'dist', // Default output directory
-//     rollupOptions: {
-//       output: {
-//         entryFileNames: 'assets/[name].js',
-//         chunkFileNames: 'assets/[name].js',
-//         assetFileNames: 'assets/[name].[ext]'
-//       }
-//     }
-//   }
 // }));
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/cocktails/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    outDir: 'dist', // Default output directory
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
+});
+
